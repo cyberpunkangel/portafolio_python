537a5a9620a538a34802e63627edc15b08459335
cyberpunkangel <149angel@hotmail.com>
2023-05-27 15:09:08 -0600

    ajustes en modal estilos y js

--- core/static/core/js/main.js
+++ core/static/core/js/main.js
@@ -1,7 +1,7 @@
 //Animacion de tecla
-var app = document.getElementById('app');
+let app = document.getElementById('app');
 
-var typewriter = new Typewriter(app, {
+let typewriter = new Typewriter(app, {
     loop: true,
     delay: 50
 });
@@ -12,6 +12,13 @@ typewriter.typeString('La inteligencia artificial no es una amenaza para la huma
     .start();
 
 //modal
+$("#exampleModal").on("shown.bs.modal", function(e) {
+    let videoIframe = $("#exampleModal iframe");
+    videoIframe.attr("src", "https://mega.nz/embed/sJFB3I5A#7AIR6DRsvY9pbdZe7CLgPLHJATEGdu9yRwPorL_Gip8");
+    videoIframe[0].load();
+});
+
 $("#exampleModal").on('hidden.bs.modal', function (e) {
-    $("#exampleModal iframe").attr("src", $("#exampleModal iframe").attr("src"));
+    let videoIframe = $("#exampleModal iframe");
+    videoIframe.attr("src", "");
 });
