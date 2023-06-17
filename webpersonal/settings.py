03c337cb5457b4fd2415d97bd10546745bead05c
cyberpunkangel <149angel@hotmail.com>
2023-06-16 23:14:37 -0600

    se agrega registro de react

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w!-l0vi-%vdc!1y=v-yhh*gxgv(3q(qlq&_$6as^a$##9j(uzz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
