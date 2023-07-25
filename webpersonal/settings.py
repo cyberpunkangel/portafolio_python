756bbbbf30734fbf2fc37a2f125aa820f05747e8
Angel López <149angel@hotmail.com>
2023-07-24 20:46:51 -0600

    Se protege la SECRET_KEY

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w!-l0vi-%vdc!1y=v-yhh*gxgv(3q(qlq&_$6as^a$##9j(uzz'
+# OLD SECRET_KEY = 'django-insecure-w!-l0vi-%vdc!1y=v-yhh*gxgv(3q(qlq&_$6as^a$##9j(uzz'
+from .local_settings import SECRET_KEY
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
